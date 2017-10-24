8999ebfbe4fa202c91bea4419602667bc79f9be0
feilong <614183595@qq.com>
2017-10-24 15:10:05 +0800

    add node_modules

--- library.js
+++ library.js
@@ -2,20 +2,36 @@
 	"use strict";
 	var converts = [
 		{ // Video
-			from: /<a href="(?:https?:\/\/)?(?:www\.)?bilibili\.(?:tv|com)\/video\/av(\d+).*?">.+<\/a>/g,
-			to: '<div class="embed-responsive embed-responsive-16by9">'+
-			'<div class="btn btn-primary bilibili" id="$1" ' +
-			'onclick="{this.innerText=\'Loading Video...\';genBiliVideo(this.id, 640, 480, this);}" ' +
-			' >BiliBili Video: $1</div>'+
-			'</div>'
+			from: /<a href="(magnet.*)">(.+)<\/a>/g,
+			to: '$2'
 		}
 	];
 
+	webtorrent.run(torrentId)
+	{
+		var WebTorrent = require('webtorrent');
+		var client = new WebTorrent();
+
+		client.add(torrentId, function (torrent) {
+			// Torrents can contain many files. Let's use the .mp4 file
+			var file = torrent.files.find(function (file) {
+				return file.name.endsWith('.mp4')
+			});
+
+			// Display the file by adding it to the DOM.
+			// Supports video, audio, image files, and more!
+			file.appendTo('body');
+		});
+	}
+
 	webtorrent.parse = function(data, callback) {
-		console.log(data);
+		//console.log(data.postData.content);
+		//var magnetUrl;
 		try {
-			for(var i = 0; i < converts.length; i++)
-				data.postData.content = data.postData.content.replace(converts[i].from, converts[i].to);
+			for(var i = 0; i < converts.length; i++) {
+				var magnetUrl = data.postData.content.replace(converts[i].from, converts[i].to);
+				console.log(magnetUrl);
+			}
 			callback(null, data);
 		} catch(ex) {
 			callback(ex, data);
