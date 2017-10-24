9aedfc8d6ebd9f503e7a756f765cdcc7fcbcd16e
feilong <614183595@qq.com>
2017-10-24 19:51:40 +0800

    in error o.o

--- library.js
+++ library.js
@@ -3,34 +3,16 @@
 	var converts = [
 		{ // Video
 			from: /<a href="(magnet.*)">(.+)<\/a>/g,
-			to: '$2'
+			to: '<div class="nodebb-webtorrent">$2</div>'
 		}
 	];
-
-	webtorrent.run(torrentId)
-	{
-		var WebTorrent = require('webtorrent');
-		var client = new WebTorrent();
-
-		client.add(torrentId, function (torrent) {
-			// Torrents can contain many files. Let's use the .mp4 file
-			var file = torrent.files.find(function (file) {
-				return file.name.endsWith('.mp4')
-			});
-
-			// Display the file by adding it to the DOM.
-			// Supports video, audio, image files, and more!
-			file.appendTo('body');
-		});
-	}
 
 	webtorrent.parse = function(data, callback) {
 		//console.log(data.postData.content);
 		//var magnetUrl;
 		try {
 			for(var i = 0; i < converts.length; i++) {
-				var magnetUrl = data.postData.content.replace(converts[i].from, converts[i].to);
-				console.log(magnetUrl);
+				data.postData.content = data.postData.content.replace(converts[i].from, converts[i].to);
 			}
 			callback(null, data);
 		} catch(ex) {
--- public/src/webtorrent.js
+++ public/src/webtorrent.js
@@ -1,1 +1,63 @@
-console.log("备用");
+var trackerList = [
+	//tracker for test
+	"http://10.5.199.42:24736",
+	"udp://10.5.199.42:24736",
+	"ws://10.5.199.42:24736",
+	//default tracker
+	'udp://tracker.openbittorrent.com:80',
+	'udp://tracker.leechers-paradise.org:6969',
+	'udp://tracker.coppersurfer.tk:6969',
+	'udp://tracker.opentrackr.org:1337',
+	'udp://explodie.org:6969',
+	'udp://zer0day.ch:1337',
+	'wss://tracker.btorrent.xyz',
+	'wss://tracker.openwebtorrent.com',
+	'wss://tracker.fastcast.nz'
+];
+
+function fixTorrent(magnetUrl)
+{
+	var magnetHash = magnetUrl.match(/btih:(\w+)/)[1];
+	var magnetName = magnetUrl.match(/dn=([^&]+)/)[1];
+	var magnetNameDecode = decodeURIComponent(magnetName);
+	var magnetTracker = encodeURIComponent(trackerList.join('&tr=')).replace(/%26/g,'&');
+
+	return ['magnet:?xt=urn:btih:' + magnetHash +
+	'&dn=' + magnetName +
+	'&tr=' + magnetTracker,
+		magnetNameDecode];
+}
+
+function nodebbwebtorrentload() {
+	var client = new WebTorrent();
+	var webtorrentdivs = $(".nodebb-webtorrent");
+	for (var i = 0; i < webtorrentdivs.length; i++) {
+		webtorrentdiv = webtorrentdivs[i];
+		torrentId = fixTorrent(webtorrentdiv.innerHTML)[0];
+
+		webtorrentdiv.innerHTML = '<p>' +
+			'<div class="nodebb-webtorrent-name">' +
+			fixTorrent(webtorrentdiv.innerHTML)[1] +
+			'</div>' +
+			'<div class="nodebb-webtorrent-magnet">' +
+			'<a src="' + fixTorrent(webtorrentdiv.innerHTML)[0] + '">' +
+			fixTorrent(webtorrentdiv.innerHTML)[0] +
+			'</a></div>' +
+			'<div class="nodebb-webtorrent-webplayer" id="nodebb-webtorrent-webplayer-"' + i + '></div>' +
+			'</p>';
+
+
+		client.add(torrentId, function (torrent) {
+			// Torrents can contain many files. Let's use the .mp4 file
+			var file = torrent.files.find(function (file) {
+				return file.name.endsWith('.mp4');
+			});
+
+			// Display the file by adding it to the DOM.
+			// Supports video, audio, image files, and more!
+			file.appendTo('#nodebb-webtorrent-webplayer-' + i);
+		})
+	}
+}
+
+$.getScript("https://cdn.jsdelivr.net/webtorrent/latest/webtorrent.min.js",nodebbwebtorrentload);
